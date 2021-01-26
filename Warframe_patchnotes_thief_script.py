ffb10c5d57abd529a114987340f7b2c660462f8f
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2021-01-26 17:15:25 -0500

    Switch from infinite loop to finite
    Also made some code cleanup

--- Warframe_patchnotes_thief_script.py
+++ Warframe_patchnotes_thief_script.py
@@ -16,37 +16,7 @@ import signal
 import sys
 
 
-
-chrome_options = webdriver.chrome.options.Options()
-if os.environ.get("GOOGLE_CHROME_BIN")!=None:
-	chromedriverpath=os.environ.get("CHROMEDRIVER_PATH")
-	chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-else:
-	chromedriverpath=r"C:\Users\Philippe\Documents\Python Scripts\Youtube Randomizer\chromedriver.exe"
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument("--disable-extensions")
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-dev-shm-usage')
-browser=webdriver.Chrome(executable_path=chromedriverpath,options=chrome_options)
-
-
-
-#Bot conditions
-bot_login=praw.Reddit(client_id = os.environ["praw_client_id"],
-	             client_secret = os.environ["praw_client_secret"],
-	             user_agent = 'warframe patch notes retriever bot 0.1',
-	             username = os.environ["praw_username"],
-	             password = os.environ["praw_password"],validate_on_submit=True)
-bot_login.validate_on_submit=True
-
-#cloudcube login
-session_cloudcube = boto3.Session(
-	aws_access_key_id=os.environ["aws_access_key_id"],
-	aws_secret_access_key=os.environ["aws_secret_access_key"],
-)
-s3 = session_cloudcube.resource('s3')
-cloud_cube_object=s3.Object('cloud-cube',os.environ["cloud_cube_file_loc"])
+#%%
 
 
 source_forum_is_updates=True #True on release
@@ -55,13 +25,47 @@ DEBUG_subreddit = False #False on release
 sort_menu_xpath='//a[@data-role="sortButton"]'
 post_date_sort_xpath='//li[@data-ipsmenuvalue="start_date"]'
 warframe_forum_url_latest_update={True:"https://forums.warframe.com/forum/3-pc-update-notes/",False:"https://forums.warframe.com/forum/36-general-discussion/"}[source_forum_is_updates]
-SUB={True:"scrappertest",False:"warframe"}[DEBUG_subreddit]
+target_SUB={True:"scrappertest",False:"warframe"}[DEBUG_subreddit]
 
 
-def post_notes(url:str,SUB_local:str):
-	with requests.session() as session:
-		response=session.get(url,timeout=20)
-		soup=BeautifulSoup(response.text,'html.parser')
+
+#%%
+
+def start_chrome_browser():
+	chrome_options = webdriver.chrome.options.Options()
+	if os.environ.get("GOOGLE_CHROME_BIN")!=None:
+		chromedriverpath=os.environ.get("CHROMEDRIVER_PATH")
+		chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+	else:
+		chromedriverpath=r"C:\Users\Philippe\Documents\Python Scripts\Youtube Randomizer\chromedriver.exe"
+	chrome_options.add_argument('--no-sandbox')
+	chrome_options.add_argument("--disable-extensions")
+	chrome_options.add_argument("--disable-gpu")
+	chrome_options.add_argument('--headless')
+	chrome_options.add_argument('--disable-dev-shm-usage')
+	return webdriver.Chrome(executable_path=chromedriverpath,options=chrome_options)
+
+
+
+def start_reddit_session():
+	bot_login=praw.Reddit(client_id = os.environ["praw_client_id"],
+		             client_secret = os.environ["praw_client_secret"],
+		             user_agent = 'warframe patch notes retriever bot 0.1',
+		             username = os.environ["praw_username"],
+		             password = os.environ["praw_password"],validate_on_submit=True)
+	bot_login.validate_on_submit=True
+	return bot_login
+
+def start_cloudcube_session():
+	session_cloudcube = boto3.Session(
+		aws_access_key_id=os.environ["aws_access_key_id"],
+		aws_secret_access_key=os.environ["aws_secret_access_key"],
+	)
+	s3 = session_cloudcube.resource('s3')
+	return s3.Object('cloud-cube',os.environ["cloud_cube_file_loc"])
+
+
+def process_div_comment(soup):
 	div_comment=soup.find('div',{"data-role":"commentContent"})
 	
 	if div_comment.find_all("blockquote"):
@@ -140,42 +144,33 @@ def post_notes(url:str,SUB_local:str):
 					for obj in ps.find_all(recursive=True):
 						obj.unwrap()
 				ps.unwrap()
+				
+	return div_comment
 
-
-
-	htt_conf=htt.HTML2Text()
-	htt_conf.use_automatic_links=True
-	htt_conf.body_width=0
-	final_post=htt_conf.handle(div_comment.decode_contents())
-	
-	#strip superfluous parts/correct mistakes
-	final_post=final_post.replace("![",'[')
-
-	#title and url
+def get_title(soup, htt_conf):
 	title_pre_split=soup.title.decode_contents()
 	title_split_index=[m.start() for m in re.finditer("-",title_pre_split)]
 	title=htt_conf.handle(title_pre_split[:title_split_index[-2]-1]).replace("PSA: ","")
 	title=title.strip()
-	if "+" in title and "PC Update Notes" in title_pre_split:
-		return
-	automatic_message="\n------\n^(This action was performed automatically, if you see any mistakes, please tag /u/desmaraisp, he'll fix them.) [^(Here is my github)](https://github.com/CephalonAhmes/CephalonAhmes)"
-	final_post="[Source]("+url+")\n\n"+final_post+automatic_message
-	soup.decompose()
+	return title, title_pre_split
 
-	subreddit_new_list=[sub_new_post.title for sub_new_post in bot_login.subreddit(SUB).new(limit=10)]
-	if title in subreddit_new_list:
-		SUB_local='scrappertest'
+def has_been_posted_to_subreddit(title, SUB):
+	subreddit_new_list=[sub_new_post.title for sub_new_post in start_reddit_session().subreddit(SUB).new(limit=10)]
+	return title in subreddit_new_list
 
+def get_subreddit_flair_id(SUB):
+	flair_template=list(start_reddit_session().subreddit(SUB).flair.link_templates)
+	return next((item.get('id') for item in flair_template if item["text"] == "News"), next((item.get('id') for item in flair_template if item["text"] == "Discussion"), None))
 
-	
+def make_submission(SUB, final_post, title, news_flair_id):
 	#Splitting and posting
-	flair_template=list(bot_login.subreddit(SUB_local).flair.link_templates)
-	news_flair_id=next((item.get('id') for item in flair_template if item["text"] == "News"), next((item.get('id') for item in flair_template if item["text"] == "Discussion"), None))
+	bot_login=start_reddit_session()
+	
 	if len(final_post)>40000:
 		split_arg=np.array([m.start() for m in re.finditer('\n\n', final_post[:40000])])[-1]
 		if split_arg==0:split_arg=np.array([m.start() for m in re.finditer('\n', final_post[:40000])])[-1]
 		final_post1,final_post2=final_post[:split_arg],final_post[split_arg:]
-		bot_login.subreddit(SUB_local).submit(title,selftext=final_post1,flair_id=news_flair_id,send_replies=False)
+		bot_login.subreddit(SUB).submit(title,selftext=final_post1,flair_id=news_flair_id,send_replies=False)
 		for submission in bot_login.redditor(os.environ["praw_username"]).new(limit=1):
 			bot_login.redditor("desmaraisp").message("Cephalon Ahmes has posted something",title+", link: "+submission.url)
 		time.sleep(5)
@@ -193,12 +188,44 @@ def post_notes(url:str,SUB_local:str):
 				break
 		
 	else:
-		bot_login.subreddit(SUB_local).submit(title,selftext=final_post,flair_id=news_flair_id,send_replies=False)
+		bot_login.subreddit(SUB).submit(title,selftext=final_post,flair_id=news_flair_id,send_replies=False)
 		for submission in bot_login.redditor(os.environ["praw_username"]).new(limit=1):
 			bot_login.redditor("desmaraisp").message("Cephalon Ahmes has posted something",title+", link: "+submission.url)
 
+def post_notes(url:str,SUB:str):
+	with requests.session() as session:
+		response=session.get(url,timeout=20)
+		soup=BeautifulSoup(response.text,'html.parser')
+	div_comment=process_div_comment(soup)
 	
-def fetch_url(forums_url_list):
+	htt_conf=htt.HTML2Text()
+	htt_conf.use_automatic_links=True
+	htt_conf.body_width=0
+	final_post=htt_conf.handle(div_comment.decode_contents())
+	
+	#strip superfluous parts/correct mistakes
+	final_post=final_post.replace("![",'[')
+
+
+	title, title_pre_split=get_title(soup, htt_conf)
+
+	if "+" in title and "PC Update Notes" in title_pre_split:
+		print("Excluded micropatch")
+		return
+	
+	automatic_message="\n------\n^(This action was performed automatically, if you see any mistakes, please tag /u/desmaraisp, he'll fix them.) [^(Here is my github)](https://github.com/CephalonAhmes/CephalonAhmes)"
+	final_post="[Source]("+url+")\n\n"+final_post+automatic_message
+	soup.decompose()
+
+	if has_been_posted_to_subreddit(title, SUB):
+		SUB="scrappertest"
+
+
+	news_flair_id= get_subreddit_flair_id(SUB)
+	make_submission(SUB, final_post, title, news_flair_id)
+
+	
+def fetch_url(forums_url_list, browser):
 	newest_urls_array=[]
 	newest_titles_array=[]
 	for forum_url in forums_url_list:
@@ -224,47 +251,63 @@ def fetch_url(forums_url_list):
 			success=True
 			newest_urls_array.append(parent_of_time_element_of_thread_list[arg_of_most_recent_thread].parent.find('a')['href'])
 			newest_titles_array.append(parent_of_time_element_of_thread_list[arg_of_most_recent_thread].parent.find('a')['title'])
-	return(np.array(newest_urls_array,dtype='<U255'),np.array(newest_titles_array,dtype='<U255'))
-	soup.decompose()
+	return np.array(newest_urls_array,dtype='<U255'),np.array(newest_titles_array,dtype='<U255')
 
-def graceful_exit(a,b):
-	browser.quit()
-	sys.exit()
+
+class signal_handler:
+	def __init__(self, browser):
+		self.browser=browser
+	def __call__(self,a,b):
+		self.browser.quit()
+		sys.exit()
+
 
 def sleep_func(sleeptime):
 	duration=2
 	for i in np.arange(0,sleeptime,duration):
 		time.sleep(duration)
 
+#%%
+def main_loop(SUB):
+	sleeptime=60
+	loop_duration_in_hours=13
+	cloud_cube_object=start_cloudcube_session()
+	browser=start_chrome_browser()
+	signal.signal(signal.SIGTERM,signal_handler(browser))
+	initial_time=time.time()
+	while (time.time()-initial_time()) < (loop_duration_in_hours*3600):
+		last_posted_urls_array=np.array(cloud_cube_object.get()['Body'].read().decode('utf-8').split('\n'),dtype='<U255')[:len(np.array(cloud_cube_object.get()['Body'].read().decode('utf-8').split('\n'),dtype='<U255'))//2]
+		last_posted_titles_array=np.array(cloud_cube_object.get()['Body'].read().decode('utf-8').split('\n'),dtype='<U255')[len(np.array(cloud_cube_object.get()['Body'].read().decode('utf-8').split('\n'),dtype='<U255'))//2:]
+		try:
+			forums_url_list=[warframe_forum_url_latest_update,'https://forums.warframe.com/forum/123-developer-workshop-update-notes/','https://forums.warframe.com/forum/170-announcements-events/']
+			newest_urls_array,newest_titles_array=fetch_url(forums_url_list, browser)
+		except TimeoutException:
+			print("Timeout")
+			sleep_func(sleeptime)
+			continue
+		for i in range(len(newest_urls_array)):
+			if newest_urls_array[i] not in last_posted_urls_array:
+				if newest_titles_array[i] not in last_posted_titles_array:
+					print(newest_titles_array[i])
+					post_notes(newest_urls_array[i],SUB)
+					last_posted_urls_array[i+2*len(forums_url_list)]=last_posted_urls_array[i+len(forums_url_list)]
+					last_posted_urls_array[i+len(forums_url_list)]=last_posted_urls_array[i]
+					last_posted_urls_array[i]=newest_urls_array[i]
+					last_posted_titles_array[i+(2*len(forums_url_list))]=last_posted_titles_array[i+len(forums_url_list)]
+					last_posted_titles_array[i+len(forums_url_list)]=last_posted_titles_array[i]
+					last_posted_titles_array[i]=newest_titles_array[i]
+		cloud_cube_object.put(Bucket='cloud-cube',Body="\n".join(np.concatenate((last_posted_urls_array,last_posted_titles_array))).encode('utf-8'),Key=os.environ["cloud_cube_file_loc"])
+		sleep_func(sleeptime)
+	print("shutting down after loop finishes")
+	signal.raise_signal(signal.SIGTERM)
+	
+	
+#%%
+
 # =============================================================================
 # post_notes("""
 # https://forums.warframe.com/topic/1238097-wrapping-up-2020-looking-toward-2021/
 # """,'scrappertest')
 # =============================================================================
-#%%
-# fetch newest pc update note post from forum
-sleeptime=60
-signal.signal(signal.SIGTERM,graceful_exit)
-while True:
-	last_posted_urls_array=np.array(cloud_cube_object.get()['Body'].read().decode('utf-8').split('\n'),dtype='<U255')[:len(np.array(cloud_cube_object.get()['Body'].read().decode('utf-8').split('\n'),dtype='<U255'))//2]
-	last_posted_titles_array=np.array(cloud_cube_object.get()['Body'].read().decode('utf-8').split('\n'),dtype='<U255')[len(np.array(cloud_cube_object.get()['Body'].read().decode('utf-8').split('\n'),dtype='<U255'))//2:]
-	try:
-		forums_url_list=[warframe_forum_url_latest_update,'https://forums.warframe.com/forum/123-developer-workshop-update-notes/','https://forums.warframe.com/forum/170-announcements-events/']
-		newest_urls_array,newest_titles_array=fetch_url(forums_url_list)
-	except TimeoutException:
-		print("Timeout")
-		sleep_func(sleeptime)
-		continue
-	for i in range(len(newest_urls_array)):
-		if newest_urls_array[i] not in last_posted_urls_array:
-			if newest_titles_array[i] not in last_posted_titles_array:
-				print(newest_titles_array[i])
-				post_notes(newest_urls_array[i],SUB)
-				last_posted_urls_array[i+2*len(forums_url_list)]=last_posted_urls_array[i+len(forums_url_list)]
-				last_posted_urls_array[i+len(forums_url_list)]=last_posted_urls_array[i]
-				last_posted_urls_array[i]=newest_urls_array[i]
-				last_posted_titles_array[i+(2*len(forums_url_list))]=last_posted_titles_array[i+len(forums_url_list)]
-				last_posted_titles_array[i+len(forums_url_list)]=last_posted_titles_array[i]
-				last_posted_titles_array[i]=newest_titles_array[i]
-	cloud_cube_object.put(Bucket='cloud-cube',Body="\n".join(np.concatenate((last_posted_urls_array,last_posted_titles_array))).encode('utf-8'),Key=os.environ["cloud_cube_file_loc"])
-	sleep_func(sleeptime)
+
+main_loop(target_SUB)
