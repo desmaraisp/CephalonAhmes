d1d9c21df02942eca7260eff4769fda2b59ee17b
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2021-02-27 13:58:45 -0500

    Fixed chromedrive path issues

--- test/test_integration.py
+++ test/test_integration.py
@@ -5,7 +5,6 @@ import numpy as np
 import os
 
 def integration_test():
-	print(os.environ)
 	browser=wpts.start_chrome_browser()
 	signal.signal(signal.SIGTERM,wpts.signal_handler(browser))
 	while True:
