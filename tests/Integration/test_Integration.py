5c5268451d207cd06ace934dc7c7b10d84cc971e
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2021-12-30 17:17:07 -0500

    Added Integration testing for full pull/parse/post cycle

--- tests/Integration/test_Integration.py
+++ tests/Integration/test_Integration.py
@@ -1,5 +1,5 @@
 import src.Warframe_patchnotes_thief_script as wpts
-import pytest
+import pytest, json
 
 @pytest.mark.integtest
 def test_Get_and_Parse_Notes():
@@ -12,4 +12,15 @@ def test_Get_and_Parse_Notes():
 	Result = wpts.Get_and_Parse_Notes(contents, "https://test.com", "TitleTest", "ForumSource")
 
 	assert Result[0] == Expected
+
+
+@pytest.mark.integtest
+def test_Pull_Parse_and_Post_Notes():
+	with open("tests/Integration/URLs_to_post.json") as file:
+		URLs_List = json.loads(file.read())
 	
+	for item in URLs_List:
+		ResponseContent = wpts.GetNotes_From_Request(item["URL"])
+		SubmissionContents, SubmussionTitle = wpts.Get_and_Parse_Notes(ResponseContent, item["URL"], item["PageName"], item["ForumPage"])
+		
+		wpts.make_submission({False:"scrappertest",True:"scrappertest"}, SubmissionContents, SubmussionTitle)
