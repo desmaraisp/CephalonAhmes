afd58f923bc30ad0ae2e6ad31fae6d0f05f56d97
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2022-01-03 17:48:04 -0500

    Add cleanup function call in signal handler

--- src/Cloud_cube_file_verification.py
+++ src/Cloud_cube_file_verification.py
@@ -3,7 +3,7 @@ import src.AhmesConfig as ahc
 import json
 
 
-Result = json.loads(wpts.fetch_cloudcube_contents(ahc.env_config["PostHistoryFileName"]))
+Result = json.loads(wpts.fetch_cloudcube_contents("PostHistory.json"))
 print(json.dumps(Result, indent=4, sort_keys=True))
 
 print("==========\n")
