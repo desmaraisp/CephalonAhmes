d24c99b9709591a299a8f4403647cce89003c847
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2022-07-11 11:11:25 -0400

    Replace f-string with .format

--- src/ConfigurationHandler.py
+++ src/ConfigurationHandler.py
@@ -4,7 +4,7 @@ import os
 current_configuration_name = os.getenv('ConfigurationName', 'Default')
 
 if(current_configuration_name not in ProjectSettings.ConfigurationClasses):
-    raise(Exception(f"No such environment : {current_configuration_name}"))
+    raise(Exception("No such environment : {}".format(current_configuration_name)))
 
 _klass = ProjectSettings.ConfigurationClasses[current_configuration_name]
 PROJECTCONFIGURATION: ProjectSettings.Default = _klass()
