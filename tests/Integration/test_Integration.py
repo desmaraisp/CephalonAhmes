f0205c51c4709db758a998442044fa4a9e9fdbc7
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2022-08-11 11:02:02 -0400

    Update output data file extension

--- tests/Integration/test_Integration.py
+++ tests/Integration/test_Integration.py
@@ -9,7 +9,7 @@ def test_Get_and_Parse_Notes():
     with open("tests/Integration/source_for_test_Get_and_Parse_Notes.html") as file:
         contents = file.read().replace("\t","").replace("\n","")
 
-    with open("tests/Integration/Expected_for_test_Get_and_Parse_Notes.html") as file:
+    with open("tests/Integration/Expected_for_test_Get_and_Parse_Notes.md") as file:
         Expected = file.read()
 
     Result = wpts.get_and_parse_notes_from_response_contents(contents, "https://test.com", "TitleTest", "ForumSource")
