10668de98f3f34a0a9f301a3847af88ad7c5480d
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2022-09-28 14:58:07 -0400

    fix: Remove selenium dependency

--- src/ParsingUtilities.py
+++ src/ParsingUtilities.py
@@ -14,9 +14,7 @@ def get_post_element_from_full_discussion(soup: BeautifulSoup):
     return soup.find('div', {"data-role": "commentContent"})
 
 def get_date_from_thread(thread):
-    time_element_of_thread = thread.findChild(
-            'time', recursive=True)['datetime']
-
+    time_element_of_thread = thread.findChild('time', recursive=True)['datetime']
     return datetime.strptime(time_element_of_thread, '%Y-%m-%dT%H:%M:%SZ')
 
 def get_latest_submission_dataclass_in_forum_page(page_source :str) -> dtc.IndividualSubmissionModel:
@@ -24,10 +22,10 @@ def get_latest_submission_dataclass_in_forum_page(page_source :str) -> dtc.Indiv
     list_of_all_threads_in_forum_page = soup.find(
             'ol', {'data-role': 'tableRows'}).find_all('div', {'class': 'ipsDataItem_main'})
 
+    list_of_all_threads_in_forum_page = [a for a in list_of_all_threads_in_forum_page if a.findChild('time', recursive=True)]
     last_thread = max(list_of_all_threads_in_forum_page, key=get_date_from_thread)
 
-    link_to_latest_post = last_thread.findChild(
-            'a', recursive=True)
+    link_to_latest_post = last_thread.findChild('a', recursive=True)
 
     return dtc.IndividualSubmissionModel(
             link_to_latest_post["title"].strip(),
