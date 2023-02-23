f725d8cd3960cfb1712588c789691e6af964bdd3
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2023-02-23 00:45:25 -0500

    test: Updated integration testing to clean up left-over comments
    Every time the tests would run, a new dud comment would get left behind, cluttering the bot's history to no end. In order to address that, we just have to call comment.delete after we're done

--- tests/Integration/test_make_submission_to_targeted_subreddit.py
+++ tests/Integration/test_make_submission_to_targeted_subreddit.py
@@ -1,5 +1,5 @@
 from typing import List
-import praw.models, os, uuid
+import praw.models, os, uuid, praw.util, prawcore, praw
 from src import (
         ConfigurationHandler as cgf,
         PrawUtilities as pru
@@ -18,10 +18,11 @@ def test_make_submission_to_targeted_subreddit() -> None:
     )
     praw_utilities: pru.PrawUtilities = pru.PrawUtilities(praw_settings)
 
-    subject = "test-submission-"+ str(uuid.uuid4())
+    generated_uuid: str = str(uuid.uuid4())
+    subject = "test-submission-"+ generated_uuid 
 
     praw_utilities.make_submission_to_targeted_subreddit(
-        submission_contents="a"*35000 + "\n\n" + "a"*9000,
+        submission_contents="a"*35000 + "\n\n" + "a"*9000 + generated_uuid,
         submission_title=subject
     )
 
@@ -34,11 +35,16 @@ def test_make_submission_to_targeted_subreddit() -> None:
     assert(len(corresponding_messages) != 0)
     session.inbox.mark_read(corresponding_messages)
 
-    submissions: List[praw.models.Submission] = session.subreddit("scrappertest").new(limit=5)
+    redditor: praw.models.Redditor = session.redditor(os.environ["CEPHALONAHMES_PRAW_USERNAME"])
+    submissions: List[praw.models.Submission] = redditor.submissions.new(limit=5)
     corresponding_submissions = [submission for submission in submissions if submission.title == subject]
 
     assert(len(corresponding_submissions) == 1)
     assert(len(corresponding_submissions[0].selftext) == 35000)
     assert(corresponding_submissions[0].num_comments > 0)
+    
+    for comment in redditor.comments.new(limit=5):
+        if comment.body == "a"*9000 + generated_uuid:
+            comment.delete()
     
     corresponding_submissions[0].delete()
