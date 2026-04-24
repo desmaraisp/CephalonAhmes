f2c298a5b32df4a6c93c37ace0726e57ed164d42
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2026-04-24 02:36:50 -0400

    refactor: remove unused client cookies

--- src/WebRequestMethods.py
+++ src/WebRequestMethods.py
@@ -1,10 +1,10 @@
 import requests
 from requests.adapters import HTTPAdapter, Retry
 
-def get_response_from_generic_url(url: str, MaxRetries: int = 5) -> requests.Response:
+def get_response_from_generic_url(url: str, max_retries: int = 5, timeout: float = 20) -> requests.Response:
     session = requests.Session()
 
-    retries = Retry(total=MaxRetries,
+    retries = Retry(total=max_retries,
                     backoff_factor=0.1,
                     status_forcelist=[ 500, 502, 503, 504 ],
                     raise_on_status=True,
@@ -12,11 +12,5 @@ def get_response_from_generic_url(url: str, MaxRetries: int = 5) -> requests.Res
 
     session.mount('http://', HTTPAdapter(max_retries=retries))
     session.mount('https://', HTTPAdapter(max_retries=retries))
-    session.cookies.set("gid", value="59aec12f3ade7fd1f103decf", domain="forums.warframe.com", path="/") # type: ignore
-    session.cookies.set("ips4_noCache", value="1", domain="forums.warframe.com", path="/") # type: ignore
-    
-    try:
-        return session.get(url, timeout=20)
-    except Exception as e:
-        raise Exception("HTTP failure for '{}'.".format(url)) from e
+    return session.get(url, timeout=timeout)
 
