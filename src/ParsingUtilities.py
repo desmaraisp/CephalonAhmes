42b34721a549c9f08e9aeca3b1b3324b6d238dcd
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2026-04-24 02:34:54 -0400

    refactor: fix minor type mismatches in RSS parser

--- src/ParsingUtilities.py
+++ src/ParsingUtilities.py
@@ -1,31 +1,56 @@
 from datetime import datetime
 import html
+from typing import List
 import html2text as htt
 from bs4 import BeautifulSoup
 from lxml import etree
 from src import(
     Models as dtc,
-    StringManipulations,
     HTMLCorrections
 )
 
 
 
-def GetLastItemInformation(value: str) -> dtc.SubmissionModel:
-    soup: etree._ElementTree = etree.fromstring(value)
-    items: etree._Element = soup.findall('.//item')
+def GetLastItemInformation(value: str) -> dtc.SubmissionModel | None:
+    soup: etree._Element = etree.fromstring(value)
+    items: List[etree._Element] = soup.findall('.//item')
     lastItem: etree._Element = max(items, key=get_date_from_node)
+    guid_text = lastItem.find('./guid')
+    if guid_text is None or guid_text.text is None:
+        return None
+
+    title_text = lastItem.find('./title')
+    if title_text is None or title_text.text is None:
+        return None
+    
+    description_text = lastItem.find('./description')
+    if description_text is None or description_text.text is None:
+        return None
+
+    link = lastItem.find('./link')
+    if link is None or link.text is None:
+        return None
+    
+    date = get_date_from_node(lastItem)
+    if date is None:
+        return None
+
     return dtc.SubmissionModel(
-        guid=int(lastItem.find('./guid').text),
-        pub_date=get_date_from_node(lastItem),
-        title=lastItem.find('./title').text,
-        contents=lastItem.find('./description').text,
-        link = lastItem.find('./link').text
+        guid=int(guid_text.text),
+        pub_date=date,
+        title=title_text.text,
+        contents=description_text.text,
+        link=link.text
     )
 
 def get_date_from_node(node: etree._Element) -> datetime:
-    pubdate: str = node.find('./pubDate').text
-    return datetime.strptime(pubdate, '%a, %d %b %Y %H:%M:%S %z')
+    pubdate = node.find('./pubDate')
+    if pubdate is None or pubdate.text is None:
+        return datetime.min
+    try:
+        return datetime.strptime(pubdate.text, '%a, %d %b %Y %H:%M:%S %z')
+    except ValueError:
+        return datetime.min
 
 def transform_contents_to_markdown(contents: str, url: str, footer:str) -> str:
     soup = BeautifulSoup(contents, 'html.parser')
